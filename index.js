cb269983802f630aeb497c63a255934b801c0d00
jerickmalungcut <jerickmalungcut@yahoo.com>
2026-01-02 21:47:39 +0800

    Finish learning

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-$("h1").css("color", "red");
+$("h1").addClass("big-title");
+
+// for (let i = 0; i < 5; i++) {
+//   document.querySelectorAll("button")[i].addEventListener("click", () => {
+//     document.querySelector("h1").style.color = "purple";
+//   });
+// }
+
+// $("button").click(() => {
+//   $("h1").css("color", "purple");
+// });
+
+// $(document).keypress((e) => {
+//   $("h1").text(e.key);
+// });
+
+// $("h1").on("mouseover", () => {
+//   $("h1").css("color", "yellow");
+// });
+
+// $("button").on("click", () => {
+//   $("h1").fadeOut().fadeIn();
+// });
+
+$("button").on("click", () => {
+  $("h1").slideUp().slideDown();
+});
